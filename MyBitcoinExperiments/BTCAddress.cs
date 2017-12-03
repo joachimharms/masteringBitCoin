825fa7c23b5870d7e56362ed5570f295129aba93
joachimharms <joachimharms77@gmail.com>
2017-12-03 23:04:55 +0100

    Backup - done

---
+++ MyBitcoinExperiments/BTCAddress.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+using NBitcoin;
+
+namespace MyBitcoinExperiments
+{
+    public class BTCAddress
+    {
+        public BitcoinAddress GetMainAddressFromPublicKey(Key privateKey)
+        {
+            PubKey publicKey = privateKey.PubKey;
+            return publicKey.GetAddress(Network.Main);
+        }
+
+        public Key GetPrivateKey()
+        {
+            Key privateKey = new Key();
+            return privateKey;
+        }
+    }
+}
---
+++ MyBitcoinExperiments/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NBitcoin;
+
+namespace MyBitcoinExperiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BTCAddress myAddress = new BTCAddress();
+            Key pk = myAddress.GetPrivateKey();
+            Console.WriteLine(myAddress.GetMainAddressFromPublicKey(pk));
+        }
+    }
+}
