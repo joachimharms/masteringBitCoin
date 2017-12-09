5f188c451b94b19a1d5d8fe8ae335e96cb4bcfca
joachimharms <joachimharms77@gmail.com>
2017-12-09 20:32:24 +0100

    Finish chapter 3 - done get block transactions code - done implement rpc calls on local node - done

--- C03_Basic_Api_Calls_NBitcoin01/Program.cs
+++ C03_Basic_Api_Calls_NBitcoin01/Program.cs
@@ -23,6 +23,48 @@ namespace C03_Basic_Api_Calls_NBitcoin01
             int blockheight = myClient.GetBlockCount();
             Console.WriteLine($"Anzahl Blöcke/Blockheight: {blockheight}");
 
+            // Example 3-4. Retrieving a transaction and iterating its outputs
+            // Alice's transaction ID
+            NBitcoin.uint256 txid = uint256.Parse("0627052b6f28912f2703066a912ea577f2ce4da4caa5a5fbd8a57286c345c2f2");
+
+            // First, retrieve the raw transaction in hex
+            string raw_tx = myClient.GetRawTransaction(txid).ToString();
+            Console.WriteLine(raw_tx);
+
+
+            // Decode the transaction hex into a JSON object
+            //Transaction decodedTx = myClient.DecodeRawTransactionAsync(raw_tx);
+            //Transaction decodedTx = myClient.DecodeRawTransaction(raw_tx);
+            //Console.WriteLine(raw_tx);
+
+            //var outputList = raw_tx.Outputs;
+            //foreach (var output in outputList)
+            //{
+            //    Console.WriteLine(output);
+            //}
+
+
+
+            // Example 3-5. Retrieving a block and adding all the transaction outputs - Seite 50
+            int blockheightOfAliceTransaction = 277316;
+
+            // Get the block hash of block with height 277316
+            var blockhash = myClient.GetBlockHash(blockheightOfAliceTransaction);
+
+            // Retrieve the block by its hash
+            var block = myClient.GetBlock(blockhash);
+            var listOfTransactions = block.Transactions;
+            Console.WriteLine($"blockheight of Alice's Transaction: {blockheightOfAliceTransaction}\n Blockhash: {blockhash}\n");
+            NBitcoin.Money sumOfTrans = 0;
+            foreach (var trans in listOfTransactions)
+            {
+                sumOfTrans += trans.TotalOut;
+            }
+            // Die Summe stimmt! Siehe Seite 51:
+            Console.WriteLine($"Bitcoin Wert insgesamt des Blocks: {sumOfTrans.ToString()}");
+
+
+
         }
     }
 }
